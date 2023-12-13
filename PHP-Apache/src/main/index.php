b15adc783ba874c44e20d7a32c9f92d2e5aedce4
AlejandroAlonsoFigueroa <alejandro.figueroa.contractor@openpay.mx>
2023-12-13 16:38:08 -0600

    Agrega maqueta del formulario de registro

---
+++ PHP-Apache/src/main/index.php
@@ -0,0 +1,67 @@
+<html>
+    <title>Práctica 1 - PHP</title>
+    <head>
+
+        <style>
+            p{
+                padding: 0;
+                margin: 0;
+            }
+            input{
+                outline: none;
+                all: unset;
+                border: solid black 1px;
+                padding: 7px 12px;
+            }
+            *{
+                font-family: Arial, Helvetica, sans-serif;
+            }
+
+            .boton{
+
+                outline: none;
+                background-color: #004481;
+                padding: 10px 20px;
+                color: white;
+                border: none;
+            }
+        </style>
+    </head>
+
+    <body>
+        <div>
+            <h1>Guarda un usuario</h1>
+        </div>
+      
+        <div style = "border: solid black 1px; width: fit-content; padding: 25px;">
+            <form>
+
+                <div>
+                    <div><p>Clave</p></div>
+                    <input type = "text" name ="clave"/>
+                </div>
+
+                <div>
+                    <div><p>Nombre</p></div>
+                    <input type = "text" name ="nombre"/>
+                </div>
+
+                <div>
+                    <div><p>Dirección</p></div>
+                    <input type = "text" name ="direccon"/>
+                </div>
+
+                <div>
+                    <div><p>Teléfono</p></div>
+                    <input type = "text" name ="telefono"/>
+                </div>
+
+                <div style ="margin-top: 10px;">
+                    <button class = "boton" type ="submit">Guardar</button>
+                </div>
+            </form>
+
+        </div>
+        
+    </body>
+</html>
